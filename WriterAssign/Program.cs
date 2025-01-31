db3663a227153fae78f994bb8b7bcd26ecc04283
Harshal Mutyalwar <harshalmutyalwar@gmail.com>
2025-01-31 14:36:38 +0530

    Writer Solution Update: Route Change in Program.cs

--- WriterAssign/Program.cs
+++ WriterAssign/Program.cs
@@ -35,7 +35,7 @@ namespace WriterAssign
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Post}/{action=Index}/{id?}");
 
             app.Run();
         }
--- WriterAssign/obj/Debug/net8.0/WriterAssign.AssemblyInfo.cs
+++ WriterAssign/obj/Debug/net8.0/WriterAssign.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WriterAssign")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aacc2d17225fda57a351e96bd000207f33be84b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("WriterAssign")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WriterAssign")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
